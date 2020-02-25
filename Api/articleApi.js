5427596b61fca1f10c39bb2d652540aa13d2e5e1
Axel <axel.brionne@gmail.com>
2020-02-25 21:08:09 +0100

    modification de l'url de base pour les appels api

--- Api/articleApi.js
+++ Api/articleApi.js
@@ -1,5 +1,5 @@
 const API_TOKEN = "iuSDG654809LKSGG.JKCGµ£A/"
-const URL_BASE = 'https://www.topicality.fr/Topicality/api/'
+const URL_BASE = 'https://www.topicality.fr/api/'
 
 export function getAllArticlesFromAPI () {
     return fetch(URL_BASE + API_TOKEN + 'article.php').then((response) => response.json())
@@ -7,6 +7,6 @@ export function getAllArticlesFromAPI () {
 }
 
 export function getImageFromAPI (name) {
-    return fetch('https://www.topicality.fr/Topicality/public/uploads/images/' + name).then((response) => response.json())
+    return fetch('https://www.topicality.fr/public/uploads/images/' + name).then((response) => response.json())
         .catch((error) => console.error(error));
 }
